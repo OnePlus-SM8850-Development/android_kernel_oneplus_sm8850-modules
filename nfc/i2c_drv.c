0c8f7c6e39c210747d1e6f0bb6a41ab65b0ba345
Khageswararao Rao B <quic_kraob@quicinc.com>
2024-12-09 16:58:06 +0530

    NFC SN300 : updated NFC driver to inline with upstream changes
    Upstream changes impacted the file operations hence updated the
    downstream SN300 nfc driver accordingly.
    
    Change-Id: I15e5e99aacdd703f6df7cc5e7423c7179672a7ac

--- nfc/i2c_drv.c
+++ nfc/i2c_drv.c
@@ -324,7 +324,9 @@ ssize_t nfc_i2c_dev_write(struct file *filp, const char __user *buf,
 
 static const struct file_operations nfc_i2c_dev_fops = {
 	.owner = THIS_MODULE,
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 12, 0))
 	.llseek = no_llseek,
+#endif
 	.read = nfc_i2c_dev_read,
 	.write = nfc_i2c_dev_write,
 	.open = nfc_dev_open,
