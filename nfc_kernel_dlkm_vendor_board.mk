f6798b7753696b71acc73a91ec515bc044eae36c
Parushuram bada <pbada@qti.qualcomm.com>
2026-02-13 15:47:20 +0530

    NFC : Enable NXP Nfc Driver compilation for Shikra
    Change-Id: I5b28d8f9ae8c0906efcbd86bef7dd7f1a98e9c25

--- nfc_kernel_dlkm_vendor_board.mk
+++ nfc_kernel_dlkm_vendor_board.mk
@@ -12,7 +12,7 @@ endif
 
 ########## Build kernel module based on local DLKM flag status ##########
 ifeq ($(NFC_DLKM_ENABLED), true)
-ifeq ($(call is-board-platform-in-list, sun canoe lahaina parrot bengal chora malabar),true)
+ifeq ($(call is-board-platform-in-list, sun canoe lahaina parrot bengal chora malabar shikra),true)
   BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/nxp-nci.ko
 endif
 endif
