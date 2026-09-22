OnePlus-SM8850-Development/android_kernel_qcom_sm8850-modules

build: Update kernel and module paths
Change-Id: I18c4a28fbfe38fd172e8f0b85baea7cf6720cb93

kleaf/repo_paths.bzl
@@ -2,8 +2,8 @@
 
 """Paths relative to the main platform workspace."""
 
-SOC_REPO_PATH = "soc-repo"
-SOC_MODULES_REPO_PATH = "vendor/qcom/opensource"
+SOC_REPO_PATH = "vendor/qcom/kernel"
+SOC_MODULES_REPO_PATH = "vendor/qcom/sm8850-modules"
 
 def soc_label(target):
     """Return a label in the configured SoC kernel package."""

OnePlus-SM8850-Development/android_kernel_oneplus_sm8850-modules

kleaf/repo_paths.bzl
@@ -2,8 +2,8 @@
 
 """Paths relative to the main platform workspace."""
 
-SOC_REPO_PATH = "soc-repo"
-SOC_MODULES_REPO_PATH = "vendor/qcom/opensource"
+SOC_REPO_PATH = "vendor/qcom/sm8850"
+SOC_MODULES_REPO_PATH = "vendor/qcom/sm8850-modules"
 
 def soc_label(target):
     """Return a label in the configured SoC kernel package."""